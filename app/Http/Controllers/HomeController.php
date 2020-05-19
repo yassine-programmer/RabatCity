10c5263a8df1558f5fca29babf468a17faaccbc1
yassine <razaq.yassine@gmail.com>
2020-05-19 22:38:30 +0000

    yassine

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -26,6 +27,13 @@ class HomeController extends Controller
     {
         $req->session()->put('role',Auth::user()->role);
         $req->session()->put('name',Auth::user()->name);
+        if (Auth::user()->role == 'admin'){
+          $users=User::all();
+            return view('home')->with('users',$users);
+        }
+
+
+
         return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard @if(Session::get('role')=='admin')<div class="small">(Admin)</div>@endif</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +13,54 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    @if(Session::get('role')=='admin')
+                    <div class="container text-center" >
+                        <button  class="btn-lg" onclick="showusers()">Afficher les utilisateurs</button>
+                            <br>
 
-                    You are logged in!
+                               <div id="users" style="display: none; margin-top: 50px;" class="container text-center ">
+                                   <table class="table table-striped">
+                                       <thead class="thead-dark">
+                                       <tr>
+                                           <th scope="col">#</th>
+                                           <th scope="col">Nom</th>
+                                           <th scope="col">Email</th>
+                                           <th scope="col">Role</th>
+                                       </tr>
+                                       </thead>
+                                       <tbody>
+
+                                           @if(isset($users))
+                                               @foreach($users as $user)
+                                                   <tr>
+                                                   <th scope="row">{{$user->id}}</th>
+                                                   <td>{{$user->name}}</td>
+                                                   <td>{{$user->email}}</td>
+                                                   <td>{{$user->role}}</td>
+                                                   </tr>
+                                                   @endforeach
+                                               @endif
+
+                                       </tbody>
+                                   </table>
+                               </div>
+                        @endif
+                        </div>
+                        <br>
+                        You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+<script>
+    function showusers() {
+        var x = document.getElementById("users");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+</script>
