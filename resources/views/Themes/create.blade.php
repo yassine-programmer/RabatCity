3385d88830848efd97c2327fdaa86ba63c9f5029
totouhama@gmail.com <OualiS4league>
2020-05-23 02:04:17 +0000

    editor

--- resources/views/Themes/create.blade.php
+++ resources/views/Themes/create.blade.php
@@ -1,5 +1,6 @@
-@extends("layouts.app")
-@section("content")
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <div>
     {!! Form::open(['action' => 'ThemesController@store', 'method' => 'post']) !!}
     <div  class="container text-center">
@@ -12,7 +13,18 @@
             <input type="text" name="Theme_intitule">
         <br>
         <b>image : </b>
-        @include('inc.StandaloneImageButton')
+
+                <h2 class="mt-4">Standalone Image Button</h2>
+                    <div class="input-group">
+                                      <span class="input-group-btn">
+                                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary text-white">
+                                          <i class="fa fa-picture-o"></i> Choose
+                                        </a>
+                                      </span>
+                        <input id="thumbnail" class="form-control" type="text" name="filepath">
+                    </div>
+                <div id="holder" style="margin-top:15px;max-height:100px;"></div>
+
             <input type="text" name="Theme_image">
         <br>
         <input type="submit" value="create">
@@ -21,4 +33,15 @@
     {!! Form::close() !!}
 
 </div>
-@endsection
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <script>
+        var route_prefix = "/filemanager";
+    </script>
+    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+    <script>
+        $('#lfm').filemanager('image', {prefix: route_prefix});
+    </script>
+
