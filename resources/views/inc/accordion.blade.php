6dbfbe136eaf5f2686e1dac82ddf1c777598c52b
totouhama@gmail.com <OualiS4league>
2020-05-29 18:14:45 +0000

    ana basl

--- resources/views/inc/accordion.blade.php
+++ resources/views/inc/accordion.blade.php
@@ -1,21 +1,21 @@
-<div class="container" id="999">
+<div class="container-fluid" id="999">
 
 
     <div class="row pt-5 pb-5">
-        <div class="col-md-7 mx-auto accordion-image" >
+        <div class="col-sm-4 mx-auto accordion-image" >
         </div>
-        <div class="col-md-5 mx-auto">
+        <div class="col-md-8 mx-auto">
             <!-- Accordion -->
             <div id="accordionExample" class="accordion shadow">
 
                 <!-- Accordion item 1 -->
                 <div class="card">
                     <div id="headingOne" class="card-header bg-white shadow-sm border-0">
-                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="d-block position-relative text-dark text-uppercase collapsible-link py-2">Collapsible Group Item #1</a></h6>
+                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="d-block position-relative text-dark text-uppercase collapsible-link py-2">Arrondissement Hay Riad</a></h6>
                     </div>
                     <div id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample" class="collapse show">
-                        <div class="card-body p-5">
-                            <p class="font-weight-light m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium error eum, ipsum nam nesciunt suscipit voluptatibus? Blanditiis explicabo neque, odio perferendis rerum similique sunt! Dicta, <possimus class=""></possimus>.</p>
+                        <div class="card-body p-5 text-center" >
+                            <iframe src="https://www.google.com/maps/d/embed?mid=1N-02HuwFxEqa2F6pJqzaQmFlzHbKY2Of" width="700" height="380"></iframe>
                         </div>
                     </div>
                 </div>
@@ -23,11 +23,11 @@
                 <!-- Accordion item 2 -->
                 <div class="card">
                     <div id="headingTwo" class="card-header bg-white shadow-sm border-0">
-                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Collapsible Group Item #2</a></h6>
+                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Arrondissement Souissi</a></h6>
                     </div>
                     <div id="collapseTwo" aria-labelledby="headingTwo" data-parent="#accordionExample" class="collapse">
-                        <div class="card-body p-5">
-                            <p class="font-weight-light m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium corporis officiis omnis tempora..</p>
+                        <div class="card-body p-5 text-center">
+                            <iframe src="https://www.google.com/maps/d/embed?mid=1T5ZgP2Osz2Mso866BTOf3eYEi92mstrn" width="700" height="380"></iframe>
                         </div>
                     </div>
                 </div>
@@ -35,11 +35,35 @@
                 <!-- Accordion item 3 -->
                 <div class="card">
                     <div id="headingThree" class="card-header bg-white shadow-sm border-0">
-                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Collapsible Group Item #3</a></h6>
+                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Arrondissement Yacoub El Mansour</a></h6>
                     </div>
                     <div id="collapseThree" aria-labelledby="headingThree" data-parent="#accordionExample" class="collapse">
-                        <div class="card-body p-5">
-                            <p class="font-weight-light m-0">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.</p>
+                        <div class="card-body p-5 text-center">
+                            <iframe src="https://www.google.com/maps/d/embed?mid=1x6mg2OxUEklR4jpVS_6gTLMt6XBXI6Hx" width="700" height="380"></iframe>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Accordion item 4 -->
+                <div class="card">
+                    <div id="headingFour" class="card-header bg-white shadow-sm border-0">
+                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Arrondissement Hassan</a></h6>
+                    </div>
+                    <div id="collapseFour" aria-labelledby="headingFour" data-parent="#accordionExample" class="collapse">
+                        <div class="card-body p-5 text-center">
+                            <iframe src="https://www.google.com/maps/d/embed?mid=169_5EE8HwqtcYd7B4VhqLE0abjJdy3ny" width="700" height="380"></iframe>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Accordion item 5 -->
+                <div class="card">
+                    <div id="headingFive" class="card-header bg-white shadow-sm border-0">
+                        <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive" class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">Arrondissement Youssoufia</a></h6>
+                    </div>
+                    <div id="collapseFive" aria-labelledby="headingFive" data-parent="#accordionExample" class="collapse">
+                        <div class="card-body p-5 text-center">
+                            <iframe src="https://www.google.com/maps/d/embed?mid=1sZWqo7QcJLL_47p3bvAoI7zqLNYZN1VS" width="700" height="380"></iframe>
                         </div>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,20 @@
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('verified');
+});
 
-Route::resource('themes','ThemesController')->middleware('verified');
-Route::get('Themes/{Theme_type}','ThemesController@afficher')->middleware('verified');
-Route::get('themes/create/{theme_type}','ThemesController@create')->middleware('verified');
+Route::resource('themes','ThemesController');
+Route::get('Themes/{Theme_type}','ThemesController@afficher');
+Route::get('themes/create/{theme_type}','ThemesController@create');
 
-Route::resource('categories','CategoriesController')->middleware('verified');
-Route::get('categories/{id}','CategoriesController@show')->middleware('verified');
-Route::get('categories/createCategorie/{id}','CategoriesController@createCategorie')->middleware('verified');
-Route::get('categories/{id}/create-sous-categorie','CategoriesController@createSousCategorie')->middleware('verified');
+Route::resource('categories','CategoriesController');
+Route::get('categories/{id}','CategoriesController@show');
+Route::get('categories/createCategorie/{id}','CategoriesController@createCategorie');
+Route::get('categories/{id}/create-sous-categorie','CategoriesController@createSousCategorie');
 
-Route::resource('articles','ArticlesController')->middleware('verified');
-Route::get('Articles/{Article_id_id}','ArticlesController@afficher')->middleware('verified');
-Route::get('articles/create/{Categorie_id}','ArticlesController@create')->middleware('verified');
+Route::resource('articles','ArticlesController');
+Route::get('Articles/{Article_id_id}','ArticlesController@afficher');
+Route::get('articles/create/{Categorie_id}','ArticlesController@create');
 
 Auth::routes(['verify' => true]);
 
