02fd779efaa2caeceaa11b55e79eedecb377e34b
totouhama@gmail.com <OualiS4league>
2020-06-02 21:19:38 +0100

    scrap

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -12,6 +12,7 @@ class ScrapingController extends Controller
 {
     public function test(Client $client)
     {
+        $Tab = array();
             $client = new Client(HttpClient::create(['timeout' => 60]));
             $crawler = $client->request('GET', 'https://www.facebook.com/pg/Conseil.Arrondissement.Agdal.Ryad/posts/?ref=page_internal');
             if($crawler->filter('._1xnd > div')->count()>0){
@@ -20,27 +21,22 @@ class ScrapingController extends Controller
                     //filtre text start
                     print 'Text : ' . $i . '<br>';
 
-                    if($post->filter('.text_exposed_root > p')->count()>0){
-                        $post->filter('.text_exposed_root > p')->each(function (Crawler $text, $i) {
-                            print $text->text();
-                            print '<br>';
-
-                        });
+                    if($post->filter('.text_exposed_root')->count()>0){
+                        print  $post->filter('.text_exposed_root')->text() ;
+                        print '<br>';
                     }
+                    else
+                        print 'text null';
                     //filtre text end
                     //filtre image begin
-                    if($post->filter('.mtm > div')->count()>0){
-                        $post->filter('.mtm > div')->each(function (Crawler $test, $i) {
-                            if($test->filter('a')->count()>0){
-                                $test->filter('a')->each(function (Crawler $image, $i) {
-                                    print 'Image : ' . $i . '<br>';
-                                    if($image->filter('img')->count()>0)
-                                    print $image->filter('img')->attr('src');
-                                    print '<br>';
-                                });
-                            }
-                        });
+                    if($post->filter('.uiScaledImageContainer > img ')->count()>0){
+                        print 'Image : ' . $i . '<br>';
+                        $r = $post->filter('.uiScaledImageContainer > img')->attr('src');
+                        print $r;
+                        print '<br>';
                     }
+                    else
+                        print 'image null';
                     //filtre image end
                     $MyPost = [];
 
